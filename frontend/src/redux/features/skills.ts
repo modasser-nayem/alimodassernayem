9fe019f3fcbc414526675dda096deefd06144c03
modasser-nayem <modassernayem@gmail.com>
2025-07-31 21:11:14 +0600

    fix frontend skill related issues

--- frontend/src/redux/features/skills.ts
+++ frontend/src/redux/features/skills.ts
@@ -6,51 +6,61 @@ const initialState: TSkill[] = [
       _id: "1",
       name: "Html",
       icon: "https://cdn-icons-png.flaticon.com/128/1051/1051277.png",
+      stack: "frontend",
    },
    {
       _id: "2",
       name: "CSS",
       icon: "https://cdn-icons-png.flaticon.com/128/732/732190.png",
+      stack: "frontend",
    },
    {
       _id: "3",
       name: "Javascript",
       icon: "https://cdn-icons-png.flaticon.com/128/5968/5968292.png",
+      stack: "frontend",
    },
    {
       _id: "4",
       name: "React",
       icon: "https://cdn-icons-png.flaticon.com/128/1126/1126012.png",
+      stack: "frontend",
    },
    {
       _id: "5",
       name: "Typescript",
       icon: "https://cdn-icons-png.flaticon.com/128/5968/5968381.png",
+      stack: "programming",
    },
    {
       _id: "6",
       name: "Node js",
       icon: "https://cdn-icons-png.flaticon.com/128/919/919825.png",
+      stack: "backend",
    },
    {
       _id: "7",
       name: "Express",
       icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-8-mv_KTf1BnBnNjaiRhNVJKBfrd-k0Q__w&usqp=CAU",
+      stack: "backend",
    },
    {
       _id: "8",
       name: "MongoDB",
       icon: "https://cdn-icons-png.flaticon.com/128/9698/9698555.png",
+      stack: "backend",
    },
    {
       _id: "9",
       name: "Tailwind CSS",
       icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuh_IjW2EY0jHuFpsXhskjYuD7GNarsDjlPQ&usqp=CAU",
+      stack: "frontend",
    },
    {
       _id: "10",
       name: "Next js",
       icon: "https://cdn-icons-png.flaticon.com/128/1126/1126012.png",
+      stack: "frontend",
    },
 ];
 
