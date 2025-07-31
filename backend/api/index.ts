8a9aa94ee74187af1b728aa3efc8f26a80ecb719
modasser-nayem <modassernayem@gmail.com>
2025-07-31 22:57:08 +0600

    fix/backend deploy issues

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,3 +1,8 @@
+import { NowRequest, NowResponse } from "@vercel/node";
 import app from "../src/app";
+import { connectDB } from "../src/app/utils/db";
 
-export default app;
+export default async function handler(req: NowRequest, res: NowResponse) {
+  await connectDB();
+  app(req as any, res as any);
+}
